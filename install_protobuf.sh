e619de111fad28156fc71139b4dc4312de996545
Olivier Fournier <olivier.fournier@usherbrooke.ca>
2023-04-06 11:50:59 -0400

    added gitignore and build folder

--- install_protobuf.sh
+++ install_protobuf.sh
@@ -1,3 +1,5 @@
+mkdir build
+cd build
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 curl -LO $PB_REL/download/v21.12/protobuf-all-21.12.zip
 unzip protobuf-all-21.12.zip
@@ -6,7 +8,7 @@ sudo apt-get install autoconf automake libtool curl make g++ unzip -y
 
 cd protobuf-21.12
 ./autogen.sh
-./configure --prefix=/usr
+./configure
 make -j$(nproc)
 sudo make install
 sudo ldconfig
