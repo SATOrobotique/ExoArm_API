3ce0ed250e561d8672bf1bd75f12bf61fca323d1
Olivier Fournier <olivier.fournier@usherbrooke.ca>
2023-04-06 10:16:37 -0400

    added protobuf install script

---
+++ install_protobuf.sh
@@ -0,0 +1,12 @@
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v21.12/protobuf-all-21.12.zip
+unzip protobuf-all-21.12.zip
+
+sudo apt-get install autoconf automake libtool curl make g++ unzip -y
+
+cd protobuf-21.12
+./autogen.sh
+./configure --prefix=/usr
+make -j$(nproc)
+sudo make install
+sudo ldconfig
